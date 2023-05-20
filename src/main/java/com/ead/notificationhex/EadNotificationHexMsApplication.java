c229366bb8cf0d9c51a0b31e8a9f7b9f9e02c27f
jehmes <jeihmesthales@hotmail.com>
2023-05-20 13:14:06 -0300

    first commit

---
+++ src/main/java/com/ead/notificationhex/EadNotificationHexMsApplication.java
@@ -0,0 +1,13 @@
+package com.ead.notificationhex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EadNotificationHexMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EadNotificationHexMsApplication.class, args);
+	}
+
+}
